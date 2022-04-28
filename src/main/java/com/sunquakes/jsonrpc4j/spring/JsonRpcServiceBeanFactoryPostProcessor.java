c4332b76d44b7e5f83495a869dbfadc4719587a8
Robert <sunquakes@outlook.com>
2022-04-28 20:33:11 +0800

    feat:.gitignore

---
+++ src/main/java/com/sunquakes/jsonrpc4j/spring/JsonRpcServiceBeanFactoryPostProcessor.java
@@ -0,0 +1,14 @@
+package com.sunquakes.jsonrpc4j.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+
+public class JsonRpcServiceBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) beanFactory;
+    }
+}
